b21538877099f72d2e1a8521fc3bba81c20c9171
einz <iljapopov01@mail.ru>
2024-10-17 16:42:41 +0300

    feat: added interfaces

--- src/models/Telegraf-bot.ts
+++ src/models/Telegraf-bot.ts
@@ -1,18 +1,38 @@
 import {Context} from "telegraf";
-import type { Update } from "telegraf/types";
-import {DaySchedule, Schedule} from "../../classes/Schedule";
+import type {Update} from "telegraf/types";
+import {DaySchedule} from "../../classes/Schedule";
+
 interface HistoryItem {
     action: string;
     match: any[];
 }
 
+interface Schedule {
+    id?: number,
+    name: string,
+    url: string,
+    checksum?: string | null,
+    schedule?: string,
+    lastUpdate?: Date
+}
+
+interface User {
+    id: number;
+    idTg: number;
+    Schedule: Schedule | null
+    scheduleId: number | null;
+}
+
 export interface SessionData {
     messageCount?: number;
     pathHistory: HistoryItem[],
-    schedule: any
+    schedule: DaySchedule[],
+    currGroupMatch: { action: string, match: [] },
+    userData: User | null
 }
 
-export interface MyContext <U extends Update = Update> extends Context<U> {
+export interface MyContext<U extends Update = Update> extends Context<U> {
     session: SessionData;
     match: any;
+    from: any;
 }
